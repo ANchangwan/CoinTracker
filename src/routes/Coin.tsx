23bc3c922826370512419d61bfdc2d5c40a9ccce
안창완 <ssdii66@gmail.com>
2024-06-13 12:49:38 +0900

    router nested

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,10 +1,35 @@
-import { spawn } from "child_process";
 import { useEffect, useState } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { Outlet, useLocation, useParams } from "react-router-dom";
 import { styled } from "styled-components";
 
 const Container = styled.div`
-  padding: 0px 50px;
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const CoinInfo = styled.div`
+  background-color: rgba(0, 0, 0, 0.5);
+  color: white;
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-radius: 15px;
+`;
+
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 28px;
+  gap: 10px;
+`;
+
+const Description = styled.p`
+  font-size: 25px;
+  margin: 20px 0px;
 `;
 
 const Header = styled.header`
@@ -24,15 +49,67 @@ const Title = styled.h1`
   color: ${(props) => props.theme.accentColor};
 `;
 
+interface InfoData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  first_data_at: string;
+  last_data_at: string;
+}
+
+interface PriceData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    USD: {
+      ath_date: string;
+      ath_price: number;
+      market_cap: number;
+      market_cap_change_24h: number;
+      percent_change_1h: number;
+      percent_change_1y: number;
+      percent_change_6h: number;
+      percent_change_7d: number;
+      percent_change_12h: number;
+      percent_change_15m: number;
+      percent_change_24h: number;
+      percent_change_30d: number;
+      percent_change_30m: number;
+      percent_from_price_ath: number;
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
+    };
+  };
+}
+
 function Coin() {
   const { coinId } = useParams();
-  const [loading, setLoading] = useState(true);
-  const [info, setInfo] = useState({});
-  const [price, setPrice] = useState({});
-  const {
-    state: { name },
-  } = useLocation();
-
+  const [loading, setLoading] = useState<boolean>(true);
+  const [info, setInfo] = useState<InfoData>();
+  const [priceInfo, setPriceInfo] = useState<PriceData>();
+  const { state } = useLocation();
   useEffect(() => {
     (async () => {
       const infoData = await (
@@ -41,18 +118,52 @@ function Coin() {
       const priceData = await (
         await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
       ).json();
+
       setInfo(infoData);
-      setPrice(priceData);
+      setPriceInfo(priceData);
       setLoading(false);
     })();
   }, []);
-
+  console.log();
   return (
     <Container>
       <Header>
-        <Title>{name || "Loading..."}</Title>
+        <Title>
+          {state?.name ? state.name : loading ? "Loading..." : info?.name}
+        </Title>
       </Header>
-      {loading ? <Loading>Loading...</Loading> : null}
+      {loading ? (
+        <Loading>Loading...</Loading>
+      ) : (
+        <>
+          <CoinInfo>
+            <OverviewItem>
+              <span>Rank:</span>
+              <span>{info?.rank}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Symbol :</span>
+              <span>{info?.symbol}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Open Source : </span>
+              <span>{info?.open_source}</span>
+            </OverviewItem>
+          </CoinInfo>
+          <Description>{info?.description}</Description>
+          <CoinInfo>
+            <OverviewItem>
+              <span>Total Suply:</span>
+              <span>{priceInfo?.total_supply}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Max Supply:</span>
+              <span>{priceInfo?.max_supply}</span>
+            </OverviewItem>
+          </CoinInfo>
+          <Outlet />
+        </>
+      )}
     </Container>
   );
 }
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -2,7 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import Root from "../Router";
 import Coins from "./Coins";
 import Coin from "./Coin";
-
+import Chart from "./Chart";
+import Price from "./Price";
 
 export const router = createBrowserRouter([
   {
@@ -16,6 +17,16 @@ export const router = createBrowserRouter([
       {
         path: "coin/:coinId",
         element: <Coin />,
+        children: [
+          {
+            path: "chart",
+            element: <Chart />,
+          },
+          {
+            path: "price",
+            element: <Price />,
+          },
+        ],
       },
     ],
   },
