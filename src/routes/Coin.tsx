2c3248eab348a50566947e2306ff8761bb5cdc82
안창완 <ssdii66@gmail.com>
2024-06-13 09:48:46 +0900

    set data

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,12 +1,60 @@
+import { spawn } from "child_process";
+import { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
+import { styled } from "styled-components";
+
+const Container = styled.div`
+  padding: 0px 50px;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Loading = styled.span`
+  text-align: center;
+  display: block;
+`;
+
+const Title = styled.h1`
+  font-size: 50px;
+  color: ${(props) => props.theme.accentColor};
+`;
 
 function Coin() {
   const { coinId } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [info, setInfo] = useState({});
+  const [price, setPrice] = useState({});
   const {
     state: { name },
   } = useLocation();
-  console.log(name);
-  return <div>coin: {coinId}</div>;
+
+  useEffect(() => {
+    (async () => {
+      const infoData = await (
+        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+      ).json();
+      const priceData = await (
+        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+      ).json();
+      setInfo(infoData);
+      setPrice(priceData);
+      setLoading(false);
+    })();
+  }, []);
+
+  return (
+    <Container>
+      <Header>
+        <Title>{name || "Loading..."}</Title>
+      </Header>
+      {loading ? <Loading>Loading...</Loading> : null}
+    </Container>
+  );
 }
 
 export default Coin;
